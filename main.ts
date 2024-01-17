2e591547cf1ab7ebb3d7a05338563453767b5df0
Francisco Rodriguez-Algarra <francisco.rodriguez.algarra@gmail.com>
2024-01-17 06:46:17 +0000

    Fully functional plugin for counting lines

--- main.ts
+++ main.ts
@@ -1,9 +1,35 @@
 import { Plugin } from 'obsidian'
 
 export default class ExamplePlugin extends Plugin { 
+	statusBarTextElement: HTMLSpanElement;
 
-  onload(): void {
-	console.log('Hello World');
-  }
+	onload() {
+		this.statusBarTextElement = this.addStatusBarItem().createEl('span');
+		this.readActiveFileAndUpdateLineCount()
 
+		this.app.workspace.on('active-leaf-change', async () => {
+			this.readActiveFileAndUpdateLineCount()
+		})
+
+		this.app.workspace.on('editor-change', editor => {
+			const content = editor.getDoc().getValue();
+			this.updateLineCount(content);
+		});
+	}
+
+	private updateLineCount(fileContent?: string) {
+		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : 0;
+		const linesWord = count === 1 ? "line" : "lines";
+		this.statusBarTextElement.textContent = `${count} ${linesWord}`;
+	}
+
+	private async readActiveFileAndUpdateLineCount() {
+		const file = this.app.workspace.getActiveFile();
+		if (file) {
+			const content = await this.app.vault.read(file);
+			this.updateLineCount(content);
+		} else {
+			this.updateLineCount(undefined);
+		}
+	}
 }
